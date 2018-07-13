a50b322c7ae58c1fd7218553c3659f3502ee08c3
chennw <chennw@lcjh.com>
2018-07-13 18:11:09 +0800

    save code

--- stream-app2/src/main/java/com/springcloud/streamapp2/SinkSender.java
+++ stream-app2/src/main/java/com/springcloud/streamapp2/SinkSender.java
@@ -1,5 +1,6 @@
 package com.springcloud.streamapp2;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
