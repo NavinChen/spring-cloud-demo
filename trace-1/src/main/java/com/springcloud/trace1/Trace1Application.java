f2f8282a5cd77666700f9ede78f03a2257470588
chennw <chennw@lcjh.com>
2018-07-16 15:40:09 +0800

    save code

---
+++ trace-1/src/main/java/com/springcloud/trace1/Trace1Application.java
@@ -0,0 +1,12 @@
+package com.springcloud.trace1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trace1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trace1Application.class, args);
+    }
+}
---
+++ trace-2/src/main/java/com/springcloud/trace2/Trace2Application.java
@@ -0,0 +1,12 @@
+package com.springcloud.trace2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trace2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trace2Application.class, args);
+    }
+}
